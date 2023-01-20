f2dbdb7cbcedb760726ea37392985ad2bc97b05e
levent <lvnt4834celebi@gmail.com>
2023-01-20 16:09:49 +0300

    son değişiklik

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 
+
+
+
+
+
 window.addEventListener("load",()=>{
-calProductTotal();
+calTotal()
 
 
 })
@@ -13,7 +18,7 @@ click.addEventListener("click", (e) => {
   if (e.target.classList.contains("minus")) {
     if (e.target.nextElementSibling.innerText > 1) {
       e.target.nextElementSibling.innerText--;
-      calProductTotal();
+      calProductTotal(e.target.closest(".elements"));
     } else {
        if (confirm("product will be remove")) {
         e.target.closest(".product").remove();
@@ -40,6 +45,7 @@ click.addEventListener("click", (e) => {
 
 let calProductTotal = (x) => {
 
+
   let price = x.querySelector("b").innerText;
 
   let quantity = x.querySelector(".amount").innerText;
@@ -55,6 +61,7 @@ let calProductTotal = (x) => {
 let calTotal = () => {
 
   let allProductTotal = document.querySelectorAll(".product-total");
+  // console.log(allProductTotal);
 
   let sumSubtotal = 0;
 
@@ -63,16 +70,16 @@ let calTotal = () => {
     
   });
 
-  
+  // console.log(sumSubtotal);
   
   
   let taxPrice =sumSubtotal  * kdv;
-  console.log(taxPrice);
+  // console.log(taxPrice);
   
-  let shippingPrice =  sumSubtotal> 0 ? shipping : 0
+  let shippingPrice =sumSubtotal> 0 ? shipping : 0
     
   
-  console.log(shippingPrice);
+  // console.log(shippingPrice);
 
  document.getElementById("subtotal").nextElementSibling.innerText = "$" + sumSubtotal.toFixed(2);
 
